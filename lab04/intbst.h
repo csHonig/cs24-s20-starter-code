d5e2d69e6b60814e97310f192d133c0aad9c1bb2
Jared Flores <jared_flores@cs.ucsb.edu>
2020-05-08 13:00:53 -0700

    Updated starter code repo with PA02

--- lab04/intbst.h
+++ lab04/intbst.h
@@ -8,9 +8,9 @@
 
 using namespace std;
 
-class IntBST{
+class IntBST {
 
-public:
+ public:
     // ctor, dtor, insert and one print method already done in intbst.cpp:
     IntBST();                   // constructor
     ~IntBST();                  // destructor
@@ -18,33 +18,33 @@ public:
     void printPreOrder() const; // prints tree data pre-order to cout
 
     // 8 METHODS YOU MUST IMPLEMENT in intbst.cpp:
-    void printInOrder() const;      // print tree data in-order to cout
-    void printPostOrder() const;    // print tree data post-order to cout
-    int sum() const;                // sum of all values
-    int count() const;              // count of values
-    bool contains(int value) const; // true if value is in tree
+    void printInOrder() const;       // print tree data in-order to cout
+    void printPostOrder() const;     // print tree data post-order to cout
+    int sum() const;                 // sum of all values
+    int count() const;               // count of values
+    bool contains(int value) const;  // true if value is in tree
 
     // THESE ARE HARDER! DO THESE LAST
-    int getPredecessor(int value) const; // returns the predecessor value of the given value or 0 if there is none
-    int getSuccessor(int value) const;   // returns the successor value of the given value or 0 if there is none
-    bool remove(int value);              // deletes the Node containing the given value from the tree
+    int getPredecessor(int value) const;       // returns the predecessor value of the given value or 0 if there is none
+    int getSuccessor(int value) const;         // returns the successor value of the given value or 0 if there is none
+    bool remove(int value);                    // deletes the Node containing the given value from the tree
 
-private:
-    struct Node
-    {
-        int info;
-        Node *left, *right, *parent;
-        // useful constructor:
-        Node(int v = 0) : info(v), left(0), right(0), parent(0) {}
+ private:
+
+    struct Node {
+	int info;
+	Node *left, *right, * parent;
+	// useful constructor:
+    Node(int v=0) : info(v), left(0), right(0), parent(0) { }
     };
 
     // just one instance variable (pointer to root node):
     Node *root;
 
     // recursive utility functions for use by public methods above
-    Node *getNodeFor(int value, Node *n) const; // IMPLEMENT THIS FIRST: returns the node for a given value or NULL if none exists
-    void clear(Node *n);                        // for destructor
-    bool insert(int value, Node *n);            // note overloading names for simplicity
+    Node* getNodeFor(int value, Node* n) const; // IMPLEMENT THIS FIRST: returns the node for a given value or NULL if none exists
+    void clear(Node *n); // for destructor
+    bool insert(int value, Node *n); // note overloading names for simplicity
     void printPreOrder(Node *n) const;
     void printInOrder(Node *n) const;
     void printPostOrder(Node *n) const;
@@ -52,8 +52,8 @@ private:
     int count(Node *n) const;
 
     // these should be used by getPredecessor and getSuccessor, and ONE of them should be used by remove
-    Node *getSuccessorNode(int value) const;   // returns the Node containing the successor of the given value
-    Node *getPredecessorNode(int value) const; // returns the Node containing the predecessor of the given value
+    Node* getSuccessorNode(int value) const;   // returns the Node containing the successor of the given value
+    Node* getPredecessorNode(int value) const; // returns the Node containing the predecessor of the given value 
 };
 
 #endif
